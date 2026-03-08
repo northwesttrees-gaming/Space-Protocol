fe77797da52546a973a504cd51bc9ed94f4686e2
NorthWestTrees <northwesttreesgaming@gmail.com>
2026-03-08 07:31:08 -0700

    Using End Generation, Command for teleporting using #d in chat, example template for asteroid feature.

---
+++ src/main/java/net/mc/spaceprotocol/procedures/QuickTPProcedure.java
@@ -0,0 +1,39 @@
+package net.mc.spaceprotocol.procedures;
+
+import net.neoforged.neoforge.event.ServerChatEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.Event;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
+
+import javax.annotation.Nullable;
+
+@EventBusSubscriber
+public class QuickTPProcedure {
+	@SubscribeEvent
+	public static void onChat(ServerChatEvent event) {
+		execute(event, event.getPlayer(), event.getRawText());
+	}
+
+	public static void execute(Entity entity, String text) {
+		execute(null, entity, text);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity, String text) {
+		if (entity == null || text == null)
+			return;
+		if ((text).equals("#d")) {
+			{
+				Entity _ent = entity;
+				if (!_ent.level().isClientSide() && _ent.getServer() != null) {
+					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
+							_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "execute in space_protocol:alien_destroyer run tp @p 0 64 0");
+				}
+			}
+		}
+	}
+}
