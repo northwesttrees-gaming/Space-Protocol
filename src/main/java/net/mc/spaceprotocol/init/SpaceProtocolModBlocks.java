f8787b20e2ba26b72fcd26ed142620b7be904079
NorthWestTrees <northwesttreesgaming@gmail.com>
2026-03-09 11:53:48 -0700

    Added Stairs, Slabs, Walls, Buttons, Pressure Plates for Block, Horizontal, and Vertical blocks. Added Panes for Grate and Bars, Added New Glass and Glass Pane Block, Sorted Alien Ship Folders

--- src/main/java/net/mc/spaceprotocol/init/SpaceProtocolModBlocks.java
+++ src/main/java/net/mc/spaceprotocol/init/SpaceProtocolModBlocks.java
@@ -29,6 +29,30 @@ public class SpaceProtocolModBlocks {
 	public static final DeferredBlock<Block> ALIEN_SHIP_WALL_LIGHT;
 	public static final DeferredBlock<Block> ALIEN_SHIP_DOOR;
 	public static final DeferredBlock<Block> ALIEN_SHIP_TRAPDOOR;
+	public static final DeferredBlock<Block> ALIEN_SHIP_BARS_PANE;
+	public static final DeferredBlock<Block> ALIEN_SHIP_GRATE_PANE;
+	public static final DeferredBlock<Block> ALIEN_SHIP_STAIRS;
+	public static final DeferredBlock<Block> ALIEN_SHIP_SLAB;
+	public static final DeferredBlock<Block> ALIEN_SHIP_WALL;
+	public static final DeferredBlock<Block> ALIEN_SHIP_BUTTON;
+	public static final DeferredBlock<Block> ALIEN_SHIP_PRESSURE_PLATE;
+	public static final DeferredBlock<Block> ALIEN_SHIP_TILE_STAIRS;
+	public static final DeferredBlock<Block> ALIEN_SHIP_TILE_SLAB;
+	public static final DeferredBlock<Block> ALIEN_SHIP_TILE_WALL;
+	public static final DeferredBlock<Block> ALIEN_SHIP_TILE_PRESSURE_PLATE;
+	public static final DeferredBlock<Block> ALIEN_SHIP_TILE_BUTTON;
+	public static final DeferredBlock<Block> ALIEN_SHIP_HORIZONTAL_STAIRS;
+	public static final DeferredBlock<Block> ALIEN_SHIP_HORIZONTAL_SLAB;
+	public static final DeferredBlock<Block> ALIEN_SHIP_HORIZONTAL_WALL;
+	public static final DeferredBlock<Block> ALIEN_SHIP_HORIZONTAL_PRESSURE_PLATE;
+	public static final DeferredBlock<Block> ALIEN_SHIP_HORIZONTAL_BUTTON;
+	public static final DeferredBlock<Block> ALIEN_SHIP_VERTICAL_STAIRS;
+	public static final DeferredBlock<Block> ALIEN_SHIP_VERTICAL_SLAB;
+	public static final DeferredBlock<Block> ALIEN_SHIP_VERTICAL_WALL;
+	public static final DeferredBlock<Block> ALIEN_SHIP_VERTICAL_PRESSURE_PLATE;
+	public static final DeferredBlock<Block> ALIEN_SHIP_VERTICAL_BUTTON;
+	public static final DeferredBlock<Block> ALIEN_SHIP_GLASS;
+	public static final DeferredBlock<Block> ALIEN_SHIP_GLASS_PANE;
 	static {
 		SELVIAN_SOLDER_ORE = register("selvian_solder_ore", SelvianSolderOreBlock::new);
 		SELVIAN = register("selvian", SelvianBlock::new);
@@ -43,6 +67,30 @@ public class SpaceProtocolModBlocks {
 		ALIEN_SHIP_WALL_LIGHT = register("alien_ship_wall_light", AlienShipWallLightBlock::new);
 		ALIEN_SHIP_DOOR = register("alien_ship_door", AlienShipDoorBlock::new);
 		ALIEN_SHIP_TRAPDOOR = register("alien_ship_trapdoor", AlienShipTrapdoorBlock::new);
+		ALIEN_SHIP_BARS_PANE = register("alien_ship_bars_pane", AlienShipBarsPaneBlock::new);
+		ALIEN_SHIP_GRATE_PANE = register("alien_ship_grate_pane", AlienShipGratePaneBlock::new);
+		ALIEN_SHIP_STAIRS = register("alien_ship_stairs", AlienShipStairsBlock::new);
+		ALIEN_SHIP_SLAB = register("alien_ship_slab", AlienShipSlabBlock::new);
+		ALIEN_SHIP_WALL = register("alien_ship_wall", AlienShipWallBlock::new);
+		ALIEN_SHIP_BUTTON = register("alien_ship_button", AlienShipButtonBlock::new);
+		ALIEN_SHIP_PRESSURE_PLATE = register("alien_ship_pressure_plate", AlienShipPressurePlateBlock::new);
+		ALIEN_SHIP_TILE_STAIRS = register("alien_ship_tile_stairs", AlienShipTileStairsBlock::new);
+		ALIEN_SHIP_TILE_SLAB = register("alien_ship_tile_slab", AlienShipTileSlabBlock::new);
+		ALIEN_SHIP_TILE_WALL = register("alien_ship_tile_wall", AlienShipTileWallBlock::new);
+		ALIEN_SHIP_TILE_PRESSURE_PLATE = register("alien_ship_tile_pressure_plate", AlienShipTilePressurePlateBlock::new);
+		ALIEN_SHIP_TILE_BUTTON = register("alien_ship_tile_button", AlienShipTileButtonBlock::new);
+		ALIEN_SHIP_HORIZONTAL_STAIRS = register("alien_ship_horizontal_stairs", AlienShipHorizontalStairsBlock::new);
+		ALIEN_SHIP_HORIZONTAL_SLAB = register("alien_ship_horizontal_slab", AlienShipHorizontalSlabBlock::new);
+		ALIEN_SHIP_HORIZONTAL_WALL = register("alien_ship_horizontal_wall", AlienShipHorizontalWallBlock::new);
+		ALIEN_SHIP_HORIZONTAL_PRESSURE_PLATE = register("alien_ship_horizontal_pressure_plate", AlienShipHorizontalPressurePlateBlock::new);
+		ALIEN_SHIP_HORIZONTAL_BUTTON = register("alien_ship_horizontal_button", AlienShipHorizontalButtonBlock::new);
+		ALIEN_SHIP_VERTICAL_STAIRS = register("alien_ship_vertical_stairs", AlienShipVerticalStairsBlock::new);
+		ALIEN_SHIP_VERTICAL_SLAB = register("alien_ship_vertical_slab", AlienShipVerticalSlabBlock::new);
+		ALIEN_SHIP_VERTICAL_WALL = register("alien_ship_vertical_wall", AlienShipVerticalWallBlock::new);
+		ALIEN_SHIP_VERTICAL_PRESSURE_PLATE = register("alien_ship_vertical_pressure_plate", AlienShipVerticalPressurePlateBlock::new);
+		ALIEN_SHIP_VERTICAL_BUTTON = register("alien_ship_vertical_button", AlienShipVerticalButtonBlock::new);
+		ALIEN_SHIP_GLASS = register("alien_ship_glass", AlienShipGlassBlock::new);
+		ALIEN_SHIP_GLASS_PANE = register("alien_ship_glass_pane", AlienShipGlassPaneBlock::new);
 	}
 
 	// Start of user code block custom blocks
--- src/main/java/net/mc/spaceprotocol/init/SpaceProtocolModItems.java
+++ src/main/java/net/mc/spaceprotocol/init/SpaceProtocolModItems.java
@@ -35,6 +35,30 @@ public class SpaceProtocolModItems {
 	public static final DeferredItem<Item> ALIEN_SHIP_WALL_LIGHT;
 	public static final DeferredItem<Item> ALIEN_SHIP_DOOR;
 	public static final DeferredItem<Item> ALIEN_SHIP_TRAPDOOR;
+	public static final DeferredItem<Item> ALIEN_SHIP_BARS_PANE;
+	public static final DeferredItem<Item> ALIEN_SHIP_GRATE_PANE;
+	public static final DeferredItem<Item> ALIEN_SHIP_STAIRS;
+	public static final DeferredItem<Item> ALIEN_SHIP_SLAB;
+	public static final DeferredItem<Item> ALIEN_SHIP_WALL;
+	public static final DeferredItem<Item> ALIEN_SHIP_BUTTON;
+	public static final DeferredItem<Item> ALIEN_SHIP_PRESSURE_PLATE;
+	public static final DeferredItem<Item> ALIEN_SHIP_TILE_STAIRS;
+	public static final DeferredItem<Item> ALIEN_SHIP_TILE_SLAB;
+	public static final DeferredItem<Item> ALIEN_SHIP_TILE_WALL;
+	public static final DeferredItem<Item> ALIEN_SHIP_TILE_PRESSURE_PLATE;
+	public static final DeferredItem<Item> ALIEN_SHIP_TILE_BUTTON;
+	public static final DeferredItem<Item> ALIEN_SHIP_HORIZONTAL_STAIRS;
+	public static final DeferredItem<Item> ALIEN_SHIP_HORIZONTAL_SLAB;
+	public static final DeferredItem<Item> ALIEN_SHIP_HORIZONTAL_WALL;
+	public static final DeferredItem<Item> ALIEN_SHIP_HORIZONTAL_PRESSURE_PLATE;
+	public static final DeferredItem<Item> ALIEN_SHIP_HORIZONTAL_BUTTON;
+	public static final DeferredItem<Item> ALIEN_SHIP_VERTICAL_STAIRS;
+	public static final DeferredItem<Item> ALIEN_SHIP_VERTICAL_SLAB;
+	public static final DeferredItem<Item> ALIEN_SHIP_VERTICAL_WALL;
+	public static final DeferredItem<Item> ALIEN_SHIP_VERTICAL_PRESSURE_PLATE;
+	public static final DeferredItem<Item> ALIEN_SHIP_VERTICAL_BUTTON;
+	public static final DeferredItem<Item> ALIEN_SHIP_GLASS;
+	public static final DeferredItem<Item> ALIEN_SHIP_GLASS_PANE;
 	static {
 		SELVIAN_SOLDER_ORE = block(SpaceProtocolModBlocks.SELVIAN_SOLDER_ORE);
 		SELVIAN = block(SpaceProtocolModBlocks.SELVIAN);
@@ -51,6 +75,30 @@ public class SpaceProtocolModItems {
 		ALIEN_SHIP_WALL_LIGHT = register("alien_ship_wall_light", AlienShipWallLightBlock.Item::new);
 		ALIEN_SHIP_DOOR = register("alien_ship_door", AlienShipDoorBlock.Item::new);
 		ALIEN_SHIP_TRAPDOOR = register("alien_ship_trapdoor", AlienShipTrapdoorBlock.Item::new);
+		ALIEN_SHIP_BARS_PANE = register("alien_ship_bars_pane", AlienShipBarsPaneBlock.Item::new);
+		ALIEN_SHIP_GRATE_PANE = register("alien_ship_grate_pane", AlienShipGratePaneBlock.Item::new);
+		ALIEN_SHIP_STAIRS = register("alien_ship_stairs", AlienShipStairsBlock.Item::new);
+		ALIEN_SHIP_SLAB = register("alien_ship_slab", AlienShipSlabBlock.Item::new);
+		ALIEN_SHIP_WALL = register("alien_ship_wall", AlienShipWallBlock.Item::new);
+		ALIEN_SHIP_BUTTON = register("alien_ship_button", AlienShipButtonBlock.Item::new);
+		ALIEN_SHIP_PRESSURE_PLATE = register("alien_ship_pressure_plate", AlienShipPressurePlateBlock.Item::new);
+		ALIEN_SHIP_TILE_STAIRS = register("alien_ship_tile_stairs", AlienShipTileStairsBlock.Item::new);
+		ALIEN_SHIP_TILE_SLAB = register("alien_ship_tile_slab", AlienShipTileSlabBlock.Item::new);
+		ALIEN_SHIP_TILE_WALL = register("alien_ship_tile_wall", AlienShipTileWallBlock.Item::new);
+		ALIEN_SHIP_TILE_PRESSURE_PLATE = register("alien_ship_tile_pressure_plate", AlienShipTilePressurePlateBlock.Item::new);
+		ALIEN_SHIP_TILE_BUTTON = register("alien_ship_tile_button", AlienShipTileButtonBlock.Item::new);
+		ALIEN_SHIP_HORIZONTAL_STAIRS = register("alien_ship_horizontal_stairs", AlienShipHorizontalStairsBlock.Item::new);
+		ALIEN_SHIP_HORIZONTAL_SLAB = register("alien_ship_horizontal_slab", AlienShipHorizontalSlabBlock.Item::new);
+		ALIEN_SHIP_HORIZONTAL_WALL = register("alien_ship_horizontal_wall", AlienShipHorizontalWallBlock.Item::new);
+		ALIEN_SHIP_HORIZONTAL_PRESSURE_PLATE = register("alien_ship_horizontal_pressure_plate", AlienShipHorizontalPressurePlateBlock.Item::new);
+		ALIEN_SHIP_HORIZONTAL_BUTTON = register("alien_ship_horizontal_button", AlienShipHorizontalButtonBlock.Item::new);
+		ALIEN_SHIP_VERTICAL_STAIRS = register("alien_ship_vertical_stairs", AlienShipVerticalStairsBlock.Item::new);
+		ALIEN_SHIP_VERTICAL_SLAB = register("alien_ship_vertical_slab", AlienShipVerticalSlabBlock.Item::new);
+		ALIEN_SHIP_VERTICAL_WALL = register("alien_ship_vertical_wall", AlienShipVerticalWallBlock.Item::new);
+		ALIEN_SHIP_VERTICAL_PRESSURE_PLATE = register("alien_ship_vertical_pressure_plate", AlienShipVerticalPressurePlateBlock.Item::new);
+		ALIEN_SHIP_VERTICAL_BUTTON = register("alien_ship_vertical_button", AlienShipVerticalButtonBlock.Item::new);
+		ALIEN_SHIP_GLASS = register("alien_ship_glass", AlienShipGlassBlock.Item::new);
+		ALIEN_SHIP_GLASS_PANE = register("alien_ship_glass_pane", AlienShipGlassPaneBlock.Item::new);
 	}
 
 	// Start of user code block custom items
